893893aca963de72f3fafa46a40db0172b5fe1d6
irenemahony <irenemahonytynagh@gmail.com>
2021-04-19 11:08:29 +0100

    git update

--- merged_df.py
+++ merged_df.py
@@ -8,6 +8,8 @@ bank_loans = pd.read_csv(r'C:\Users\paudi\Desktop\Data Analytics\.csv files UCD 
 print(house_sales.head())
 print(bank_loans.head())
 
+# Objective of merged info is to to see of All property transactions what portion was mortgage approved v's Cash- buyer (using Value & count)
+# Removed New Home v's other as not going to look at this subgroup.
 # Not using Concat as not common columns that would allow stack merge.
 # No need to specify Inner Join as default setting of .merge function.
 merged_df = house_sales.merge(bank_loans, on="Year", suffixes=('_house_sales', '_bank_loans'))
@@ -15,17 +17,31 @@ print(merged_df.head(6))
 print(merged_df.columns)
 print(merged_df.dtypes)
 
-# Objective of merged info is to to see of All property transactions what portion was mortgage approved v's Cash- buyer (using Value & count)
-# Rename columns to make more sense of combined dataframe - removed New Home v's other as not going to look at this subgroup.
 
+# Note the value currency is recorded differently in DF1 and DF2 - need to amend DF2 Total Value column (4141.8m) to mirror DF1 Column (5.1m)
+merged_df["Total Value_bank_loans"] = merged_df["Total Value_bank_loans"]*1000000
 
-# May be useful once Merged in Next Dataset
-fig, ax = plt.subplots()
-ax.plot(merged_df["Year"], merged_df["Total No._house_sales"], color='purple')
-ax.set_xlabel("Year")
-ax.set_ylabel("No of Houses Sold")
-ax2 = ax.twinx()
-ax.plot(merged_df["Year"], merged_df["Total No._bank_loans"], color="red")
-ax.set_title("Total Property Sales v's Mortgage Drawdowns")
+# Fig 8: NP Array conversion to illustrate comparable transaction count & value - matplotlib
+x = np.array(merged_df["Year"])
+y0 = np.array(merged_df["Total No._house_sales"])
+y1 = np.array(merged_df["Total No._bank_loans"])
+y2 = np.array(merged_df["Total Value_house_sales"])
+y3 = np.array(merged_df["Total Value_bank_loans"])
+
+plt.style.use('seaborn-darkgrid')
+fig, (ax, ax1) = plt.subplots(2, 1)
+axs = (ax, ax1)
+
+ax.plot(x, y0, color='purple', label="Total Houses Sold", linestyle='solid')
+ax.plot(x, y1, color="red", label="Total Mortgage Drawdowns", linestyle='dashed')
+ax1.plot(x, y2, color='purple', label="Total Value of Houses Sold", linestyle='solid')
+ax1.plot(x, y3, color="red", label="Total Value Mortgage Drawdowns", linestyle='dashed')
+
+ax.set(ylabel='No of Houses Sold')
+ax1.set(xlabel='Year', ylabel="Value of House Sold (€ Millions)")
+ax.set_title("Total House Sales v's Mortgage drawdowns")
+ax1.set_title("Total Value of House Sales v's Mortgage drawdowns")
+ax.legend()
+ax1.legend()
 plt.show()
 
